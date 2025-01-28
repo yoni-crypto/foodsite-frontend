9d63e048e4f42dd6a05aee86c12d57e83f0920ee
yoni-crypto <yonidisu111@gmail.com>
2025-01-28 19:14:51 +0300

    changed localhost

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext=createContext(null)
 const StoreContextProvider=(props)=>{
 
     const[cartItems,setCartItems]=useState({})
-    const url="http://localhost:4000"
+    const url="https://foodsite-backend.vercel.app/"
     const [token,setToken]=useState("")
     const [Food_list,setFoodList]=useState([])
 
