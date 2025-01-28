99bdd26431c4b328be5d1286d3261a3284dd195f
yoni-crypto <yonidisu111@gmail.com>
2025-01-28 23:43:02 +0300

    changed localhost

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -4,44 +4,35 @@ import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
 const FoodItem = ({ _id, name, price, description, image }) => {
-    const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
-
+    const {  cartItems,addToCart, removeFromCart ,url} = useContext(StoreContext);
+    // console.log({id})
     return (
         <div className="food-item">
             <div className="food-item-image-container">
-                <img
-                    className="food-item-image"
-                    src={image}
-                    alt={name}
-                />
+                <img className="food-item-image" src={image} alt="" />
                 {!cartItems[_id] ? (
-                    <img
-                        className="add"
-                        onClick={() => addToCart(_id)}
-                        src={assets.add_icon_white}
-                        alt="Add"
-                        style={{ width: "30px" }}
-                    />
+                    <img className='add' onClick={() => addToCart(_id)} src={assets.add_icon_white} alt='' style={{width:"30px"}}/>
                 ) : (
-                    <div className="food-item-counter">
+                    <div className="food-item-Counter">
                         <img
                             onClick={() => removeFromCart(_id)}
                             src={assets.remove_icon_red}
-                            alt="Remove"
+                            alt=""
                         />
                         <p>{cartItems[_id]}</p>
                         <img
                             onClick={() => addToCart(_id)}
                             src={assets.add_icon_green}
-                            alt="Add"
+                            alt=""
                         />
+                        
                     </div>
                 )}
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
                     <p>{name}</p>
-                    <img src={assets.rating_stars} alt="Rating" />
+                    <img src={assets.rating_starts} alt="" />
                 </div>
                 <p className="food-item-desc">{description}</p>
                 <p className="food-item-price">${price}</p>
